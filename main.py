785554173261aec9e9071bc46ef107800de6fd31
Mahammadhanzla <161584945+hanzla-mansuri@users.noreply.github.com>
2026-04-28 09:18:57 +0300

    Initial commit: environment setup with data science libraries

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import (precision_score, recall_score, roc_curve, 
+                             auc, precision_recall_curve, average_precision_score)
+import kagglehub
+import os
+
+print("Environment is ready!")
